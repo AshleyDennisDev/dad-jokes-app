eecf65c01fcfb4bf637a65f825d864d051370b2f
Ashley Dennis <adenn1852@gmail.com>
2023-08-12 19:36:37 -0400

    created app, getting jokes

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+<h1>Have Some Chuckles</h1>
+<Jokes/>
+</div>
+</template>
+
+<script>
+import Jokes from "./components/Jokes.vue";
+
+export default{
+  components: {
+     Jokes,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Jokes.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <p>{{ getJoke }}</p>
+        <button @click="getRandomJoke">New Joke</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "Jokes",
+    data () {
+    return {
+      getJoke: '',
+      error: null
+    }
+  },
+  mounted(){
+    this.getRandomJoke()
+  },
+  methods: {
+    async getRandomJoke() {
+        try{
+        const response = await fetch("https://icanhazdadjoke.com/",{
+          headers: {
+            'Accept': 'application/json',
+          },
+        });
+        const  data  = await response.json()
+        this.getJoke = data.joke
+        }catch (error){
+            console.error('Error fetching joke:', error);
+            alert("Error fetching jokes")  
+        }
+    }
+
+  },
+}
+</script>
+
+<style>
+</style>
